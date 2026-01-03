e83b559828139635454de56d3520306351626bfe
Ruan <slidruan@gmail.com>
2026-01-03 17:59:43 -0300

    novo ensinamento

---
+++ ex001/script.js
@@ -0,0 +1,8 @@
+/*console.log("ola")
+window.alert ('ola world')
+window.confirm('vc é gay?')
+window.prompt ('nome?') */
+
+var dado1 = 'car1'
+let dado2 = 'car2'
+var dado3 = `car 3`
---
+++ ex008/ex008.js
@@ -0,0 +1,8 @@
+let vel = 20
+console.log('Digite a sua Velocidade')
+console.log(`Sua velocidade é ${vel} Km/h`)
+if (vel>=60) {
+    console.log('Você ultrapassou a velocidade!')
+} else {
+    console.log('A sua velocidade está boa, continue assim!')
+}
