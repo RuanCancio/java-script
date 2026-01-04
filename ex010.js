a0f555afc71d90c381383f3e11bfed3db6be145c
Ruan <slidruan@gmail.com>
2026-01-04 13:01:37 -0300

    novo commit

---
+++ ex010.js
@@ -0,0 +1,8 @@
+let hora = 15
+console.log(hora)
+if(hora<=12) {
+    console.log('Bom dia!')}
+    else if (hora < 18) {console.log('Boa Tarde!')}
+        else if(hora >= 18) {
+            console.log ('Boa noite Rapazeada!')
+    }
