c1a41f4e5694f656ff206b704d4e8e2e4432eb28
Ruan <slidruan@gmail.com>
2026-01-10 23:35:52 -0300

    Aprendendo mais sobre api's, ajax e json

--- My projects/Sistema Modular/main.js
+++ My projects/Sistema Modular/main.js
@@ -1,7 +1,16 @@
-import {soma} from "./calculadora.js"
+/*import {soma} from "./calculadora.js"
 import { media, situacao } from "./aluno.js"
 
 
 console.log(soma(10, 5))
 console.log(media(6, 7, 8))
-console.log(situacao(7))
+console.log(situacao(7))*/
+
+let produto = {
+  nome: "Teclado",
+  preco: 150,
+  emEstoque: true
+}
+
+produto = JSON.stringify(produto)
+console.log(produto)
